d70f57aa79078771002b1d1a807a5f95ce580efd
Pavel Naydysh <pav3ln_work@mail.ru>
2023-06-19 19:32:48 +0300

    Стрельба и смена оружия
    Стрельба и смена оружия

---
+++ Arena Moroka/Assets/Scripts/AutoGunController.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+
+public class AutoGunController : MonoBehaviour
+{
+    [Header("Привязка камеры")]
+    public Camera mainCamera;
+
+    [Header("Точка выстрела")]
+    public Transform firePoint;
+
+    [Header("Звук выстрела")]
+    public AudioClip shootSound;
+
+    [Header("Эффект попадания по земле")]
+    public GameObject groundHitEffectPrefab;
+    public float groundHitEffectDuration;
+
+    [Header("Настройки оружия")]
+    public float fireRange;
+    public float fireRate; 
+    public int magazineSize;
+    public float reloadTime;
+
+    private AudioSource audioSource;
+    private float lastFireTime;
+    private int currentAmmo;
+    private bool isReloading;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        currentAmmo = magazineSize;
+    }
+
+    private void Update()
+    {
+        if (isReloading)
+        {
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && CanShoot())
+        {
+            Shoot();
+            lastFireTime = Time.time;
+
+            currentAmmo--;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && currentAmmo < magazineSize)
+        {
+            StartReload();
+        }
+
+        AlignWeapon();
+    }
+
+    private bool CanShoot()
+    {
+        return Time.time - lastFireTime >= 1f / fireRate && currentAmmo > 0;
+    }
+
+    private void Shoot()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        audioSource.PlayOneShot(shootSound);
+
+        if (Physics.Raycast(ray, out hit, fireRange))
+        {
+            Debug.Log("Попадание в объект: " + hit.collider.name);
+
+            GameObject hitEffect = Instantiate(groundHitEffectPrefab, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(hitEffect, groundHitEffectDuration);
+        }
+        else
+        {
+            Debug.Log("Промах");
+        }
+    }
+
+    private void AlignWeapon()
+    {
+        Vector3 cameraDirection = mainCamera.transform.forward;
+        transform.rotation = Quaternion.LookRotation(cameraDirection);
+    }
+
+    private void StartReload()
+    {
+        if (!isReloading && currentAmmo < magazineSize)
+        {
+            Debug.Log("Перезарядка...");
+
+            isReloading = true;
+            currentAmmo = 0;
+
+            Invoke("FinishReload", reloadTime);
+        }
+    }
+
+    private void FinishReload()
+    {
+        Debug.Log("Перезарядка завершена");
+
+        isReloading = false;
+        currentAmmo = magazineSize;
+    }
+}
---
+++ Arena Moroka/Assets/Scripts/GunSwitch.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class GunSwitch : MonoBehaviour
+{
+    [Header("Виды оружия")]
+    public GameObject[] weapons; // Массив с оружием
+
+    private int currentWeaponIndex = 0;
+
+    private void Start()
+    {
+        SwitchWeapon(currentWeaponIndex); // Устанавливаем начальное оружие
+    }
+
+    private void Update()
+    {
+        int previousWeaponIndex = currentWeaponIndex;
+
+        // Проверяем нажатие клавиш "1" и "2"
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeaponIndex = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeaponIndex = 1;
+        }
+
+        // Если выбранное оружие изменилось, переключаем его
+        if (currentWeaponIndex != previousWeaponIndex)
+        {
+            SwitchWeapon(currentWeaponIndex);
+        }
+    }
+
+    private void SwitchWeapon(int weaponIndex)
+    {
+        // Деактивируем все оружие
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            weapons[i].SetActive(false);
+        }
+
+        // Активируем выбранное оружие
+        weapons[weaponIndex].SetActive(true);
+    }
+}
