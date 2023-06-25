312514a3b76d58c32b1d86385dbb4d944bae63c4
starosipticon <starosika.tema@yandex.ru>
2023-06-26 01:39:59 +0300

    правки

--- Arena Moroka/Assets/Scripts/FloatingText.cs
+++ Arena Moroka/Assets/Scripts/FloatingText.cs
@@ -6,11 +6,16 @@ public class FloatingText : MonoBehaviour
 {
     public float DestroyTime = 3f;
     public Vector3 Offset = new Vector3(0, 2, 10);
+    public Vector3 RandomizeIntensity = new Vector3(10, 0, 0);
     void Start()
     {
         
         Destroy(gameObject, DestroyTime);
         transform.localPosition += Offset;
+        transform.localPosition += new Vector3(
+            Random.Range(-RandomizeIntensity.x, RandomizeIntensity.x),
+            Random.Range(-RandomizeIntensity.y, RandomizeIntensity.y),
+            Random.Range(-RandomizeIntensity.z, RandomizeIntensity.z));
     }
     void Update()
     {
